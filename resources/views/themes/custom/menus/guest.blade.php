51be2260638892e8df11999bf0f96268bb3598a3
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2021-12-16 10:08:44 +0100

    Fixed header styling

--- resources/views/themes/custom/menus/guest.blade.php
+++ resources/views/themes/custom/menus/guest.blade.php
@@ -4,12 +4,12 @@
         {{ __("Blog") }}
     </a>
     
-    <a href="{{ route('login') }}" class="text-base font-medium leading-6 text-white whitespace-no-wrap hover:underline focus:outline-none focus:text-gray-900">
+    <a href="{{ route('login') }}" class="text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:underline focus:outline-none focus:text-wave-600">
         {{ __("Log In") }}
     </a>
     <span class="inline-flex rounded-md shadow-sm">
-        <a href="{{ route('register') }}" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-wave-500 hover:bg-wave-600 focus:outline-none focus:border-indigo-700 focus:shadow-outline-wave active:bg-wave-700">
+        <button href="{{ route('register') }}" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-wave-500 hover:bg-wave-600 focus:outline-none focus:border-indigo-700 focus:shadow-outline-wave active:bg-wave-700">
             {{ __("Register") }}
-        </a>
+        </button>
     </span>
 </nav>
--- resources/views/themes/custom/partials/notifications.blade.php
+++ resources/views/themes/custom/partials/notifications.blade.php
@@ -4,7 +4,7 @@
     @php $unreadNotifications = auth()->user()->unreadNotifications->take(5); @endphp
     <div id="notification-list" @click.away="open = false" class="relative flex items-center h-full" x-data="{ open: false }">
         <div id="notification-icon relative">
-            <button @click="open = !open" class="relative p-1 ml-3 transition duration-150 ease-in-out rounded-full hover:text-gray-300 focus:outline-none focus:text-gray-500 focus:bg-gray-100">
+            <button @click="open = !open" class="relative p-1 ml-3 text-gray-400 transition duration-150 ease-in-out rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100">
                 <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
                 </svg>
