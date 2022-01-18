6da3ec1cf7950d642340dbb3211706f9d1f5c937
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2022-01-18 15:47:14 +0100

    Added Ubuntu font

--- resources/views/themes/custom/page.blade.php
+++ resources/views/themes/custom/page.blade.php
@@ -4,14 +4,14 @@
 
 
     <div class="py-20 mx-auto max-w-6xl">
-        <article id="page-{{ $page->id }}" class="max-w-4xl px-5 mx-auto prose prose-xl lg:prose-2xl lg:px-0">
+        <article id="page-{{ $page->id }}" class="max-w-4xl px-5 mx-auto prose prose-xl lg:px-0">
 
             <meta property="name" content="{{ $page->title }}">
             <meta property="author" typeof="Person" content="admin">
             <meta property="dateModified" content="{{ Carbon\Carbon::parse($page->updated_at)->toIso8601String() }}">
             <meta class="uk-margin-remove-adjacent" property="datePublished" content="{{ Carbon\Carbon::parse($page->created_at)->toIso8601String() }}">
 
-            <h1>{{ $page->title }}</h1>
+            <h2 class="mt-0">{{ $page->title }}</h2>
 
             @if(!is_null($page->image))
                 <img width="1200" height="640" src="{{ $page->image() }}" alt="{{ $page->title }}" srcset="{{ $page->image() }}">
