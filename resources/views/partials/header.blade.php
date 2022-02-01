ef8d24c5d70912728a8a4b6fbd69a713479a772c
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2022-02-01 15:21:02 +0100

    Spacing fix in header logo; rename scss frontend file

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header id="header" x-data="{ mobileMenuOpen: false }" class="border-b-2 border-gray-100 relative z-30 gradient-border-b">
-    <div class="px-3 md:px-8 mx-auto xl:px-0 max-w-6xl border-b-1 border-[#cecece8c]">
+    <div class="px-3 py-2 md:px-8 mx-auto xl:px-0 max-w-6xl border-b-1 border-[#cecece8c]">
         <div class="flex items-center justify-between h-24 md:justify-start md:space-x-6">
             <div class="inline-flex">
                 <a id="header-logo-link" href="{{ route('home') }}" class="flex items-center justify-center space-x-3 transition-all duration-1000 ease-out transform text-gray-900">
