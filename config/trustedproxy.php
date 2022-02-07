049fd37ec0a1ee79b7864f0614276711aa4d9d6a
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2022-02-07 17:06:15 +0100

    Fixed explode() second argument (not null)

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -15,7 +15,7 @@ return [
      * of your proxy (e.g. if using ELB or similar).
      *
      */
-    'proxies' => explode(',', env('TRUSTED_PROXIES', null)), // [<ip addresses>,], '*', '<ip addresses>,'
+    'proxies' => explode(',', env('TRUSTED_PROXIES', '')), // [<ip addresses>,], '*', '<ip addresses>,'
 
     /*
      * To trust one or more specific proxies that connect
