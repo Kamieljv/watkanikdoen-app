bd6ffd7649a7a1daf9f79d04012f87b0b51d1bbf
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2022-03-01 17:13:21 +0100

    Updated user password in seeders

--- database/seeders/AnnouncementUserTableSeeder.php
+++ database/seeders/AnnouncementUserTableSeeder.php
@@ -29,21 +29,6 @@ class AnnouncementUserTableSeeder extends Seeder
                 'announcement_id' => 2,
                 'user_id' => 1,
             ),
-            2 => 
-            array (
-                'announcement_id' => 1,
-                'user_id' => 2,
-            ),
-            3 => 
-            array (
-                'announcement_id' => 2,
-                'user_id' => 2,
-            ),
-            4 => 
-            array (
-                'announcement_id' => 3,
-                'user_id' => 2,
-            ),
             5 => 
             array (
                 'announcement_id' => 3,
