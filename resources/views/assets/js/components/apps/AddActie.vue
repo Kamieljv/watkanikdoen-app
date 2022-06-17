7595719bb241da5a40b1990c26b8220406a5a651
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2022-06-17 20:45:46 +0200

    Small fix in AddActie flow

--- resources/views/assets/js/components/apps/AddActie.vue
+++ resources/views/assets/js/components/apps/AddActie.vue
@@ -144,7 +144,7 @@ export default {
         report: {},
         selectedOrganizers: [],
         currentUser: {},
-        sent: true,
+        sent: false,
         currentErrors: [],
     }),
     computed: {
