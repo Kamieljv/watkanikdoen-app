9d137724f70150125a2e2de6c06a37ea082975be
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2022-06-17 21:42:26 +0200

    Fix cookie acceptance on mobile

--- resources/views/partials/cookies.blade.php
+++ resources/views/partials/cookies.blade.php
@@ -6,19 +6,19 @@
             @svg('custom-phosphor-cookie-light', ['style' => 'fill: currentColor; height: 32px; width: auto; display: inline;'])
             We gebruiken cookies
         </h3>
-        <div class="mb-2 flex justify-between">
+        <div class="mb-2 flex justify-between items-center">
             <div>
                 We gebruiken uitsluitend functionele en analytische cookies (die u niet kunt weigeren) en delen geen data met derden.
                 Lees meer over onze cookies in de
                 <a href="/voorwaarden-en-privacyverklaring" target="_blank" class="font-bold hover:underline">Voorwaarden en Privacyverklaring</a>.
                 Bij bezoek aan deze website gaat u hiermee akkoord.
             </div>
-            <button id="acceptCookies" class="hidden md:block px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-gray-700 hover:bg-gray-900">
+            <button class="acceptCookiesBtn hidden md:block px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-gray-700 hover:bg-gray-900">
                 Oké
             </button>
         </div>
         <div class="mt-5 flex justify-end space-x-3">
-            <button id="acceptCookies" class="block md:hidden px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-gray-700 hover:bg-gray-900">
+            <button class="acceptCookiesBtn block md:hidden px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-gray-700 hover:bg-gray-900">
                 Oké
             </button>
             {{-- <button id="rejectCookies" class="px-4 py-2 text-base font-medium leading-6 text-gray-800 whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-gray-200 hover:bg-gray-300">
