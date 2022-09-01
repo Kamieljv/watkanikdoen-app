09e3d5531366b96f78c76e37d02acfcc836e3175
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2022-09-01 17:11:17 +0200

    Clearable inputs

--- resources/views/assets/js/components/ActieAgenda.vue
+++ resources/views/assets/js/components/ActieAgenda.vue
@@ -9,15 +9,16 @@
 					:isOpen="false"
 				>
 					<div id="filter-wrapper" class="col grid gap-3 grid-cols-2 sm:grid-cols-3 md:grid-cols-5">
-						<div>
-							<t-input
-								type="text"
-								placeholder="Zoeken..."
-								:autofocus="true"
-								v-model="query"
-								@input="processQuery"
-							/>
-						</div>
+						<FormField
+                            v-model="query"
+							:value="query"
+                            name="query"
+                            type="text"
+							placeholder="Zoeken..."
+                            @input="processQuery"
+							:clearable="true"
+							classes="block w-full h-full px-3 py-2 transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed text-black placeholder-gray-400 bg-white border-gray-300 focus:border-blue-500"
+                        />
 						<t-rich-select
 							id="theme-selector"
 							:options="themes"
