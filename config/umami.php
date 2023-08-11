c65bcd023a3f153ee10bc8dba4e09a22c7ff0988
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2023-08-11 18:10:39 +0200

    Update umami websiteId

--- config/umami.php
+++ config/umami.php
@@ -3,6 +3,7 @@
 return [
     'url' => env('UMAMI_URL'),
     'key' => env('UMAMI_KEY'),
+    'websiteId' => env('UMAMI_WEBSITE_ID'),
     'username' => env('UMAMI_USERNAME'),
     'password' => env('UMAMI_PASSWORD'),
 ];
