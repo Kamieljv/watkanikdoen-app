a2aa90c1f060c08832df97bcd206e551506fb335
Kamiel Verhelst <kamiel.verhelst@gmail.com>
2023-11-06 21:25:08 +0100

    Fix leaflet icon

--- resources/js/components/CoordinatesFormField.vue
+++ resources/js/components/CoordinatesFormField.vue
@@ -75,13 +75,13 @@
 </template>
 
 <script>
-// fix marker shadows
+// fix marker assets
 import * as L from "leaflet"
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
-	iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
-	iconUrl: require("leaflet/dist/images/marker-icon.png").default,
-	shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 })
 
 // Load formfield
