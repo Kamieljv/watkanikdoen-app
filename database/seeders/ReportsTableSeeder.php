053b0a0293458ae4a966c1219afaa5f5685bb75d
Kamieljv <kamiel.verhelst@proton.me>
2024-03-22 13:14:09 +0100

    Made migrations reversible; merged migrations; updated seeders for reports and acties

--- database/seeders/ReportsTableSeeder.php
+++ database/seeders/ReportsTableSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use DB;
 use Illuminate\Database\Seeder;
 
 class ReportsTableSeeder extends Seeder
 {
+
     /**
      * Auto generated seed file
      *
@@ -14,68 +14,76 @@ class ReportsTableSeeder extends Seeder
      */
     public function run()
     {
+        
 
-
-        DB::table('reports')->delete();
-
-        DB::table('reports')->insert(array (
-            0 =>
+        \DB::table('reports')->delete();
+        
+        \DB::table('reports')->insert(array (
+            0 => 
             array (
+                'actie_id' => NULL,
+                'body' => '<p>Dit is een test</p>',
+                'created_at' => '2022-01-19 16:22:36',
+                'end_date' => '2022-01-19',
+                'end_time' => '17:22:00',
+                'externe_link' => '#',
                 'id' => 1,
-                'user_id' => 1,
+                'image' => NULL,
+                'location' => NULL,
+                'location_human' => 'Loc',
                 'organizer_ids' => '1',
-                'title' => 'Test report',
-                'body' => '<p>Dit is een test</p>',
-                'externe_link' => '#',
+                'reporter_notified' => NULL,
                 'start_date' => '2022-01-19',
                 'start_time' => '17:22:00',
-                'end_date' => '2022-01-19',
-                'end_time' => '17:22:00',
-                'location' => null,
-                'location_human' => 'Loc',
-                'image' => null,
                 'status' => 'PENDING',
-                'created_at' => '2022-01-19 16:22:36',
+                'title' => 'Test report',
                 'updated_at' => '2022-01-21 10:35:43',
+                'user_id' => 1,
             ),
-            1 =>
+            1 => 
             array (
+                'actie_id' => NULL,
+                'body' => '<p>Dit is een test</p>',
+                'created_at' => '2022-01-19 16:22:36',
+                'end_date' => '2022-01-19',
+                'end_time' => '17:22:00',
+                'externe_link' => '#',
                 'id' => 2,
-                'user_id' => 1,
+                'image' => NULL,
+                'location' => NULL,
+                'location_human' => 'Loc',
                 'organizer_ids' => '1',
-                'title' => 'Test report 2',
-                'body' => '<p>Dit is een test</p>',
-                'externe_link' => '#',
+                'reporter_notified' => NULL,
                 'start_date' => '2022-01-19',
                 'start_time' => '17:22:00',
-                'end_date' => '2022-01-19',
-                'end_time' => '17:22:00',
-                'location' => null,
-                'location_human' => 'Loc',
-                'image' => null,
                 'status' => 'APPROVED',
-                'created_at' => '2022-01-19 16:22:36',
+                'title' => 'Test report 2',
                 'updated_at' => '2022-01-21 10:35:43',
-            ),
-            2 =>
-            array (
-                'id' => 3,
                 'user_id' => 1,
-                'organizer_ids' => '1',
-                'title' => 'Test report 3',
+            ),
+            2 => 
+            array (
+                'actie_id' => NULL,
                 'body' => '<p>Dit is een test</p>',
-                'externe_link' => '#',
-                'start_date' => '2022-01-19',
-                'start_time' => '17:22:00',
+                'created_at' => '2022-01-23 15:42:24',
                 'end_date' => '2022-01-19',
                 'end_time' => '17:22:00',
-                'location' => null,
+                'externe_link' => '#',
+                'id' => 3,
+                'image' => NULL,
+                'location' => NULL,
                 'location_human' => 'Loc',
-                'image' => null,
+                'organizer_ids' => '1',
+                'reporter_notified' => NULL,
+                'start_date' => '2022-01-19',
+                'start_time' => '17:22:00',
                 'status' => 'REJECTED',
-                'created_at' => '2022-01-23 15:42:24',
+                'title' => 'Test report 3',
                 'updated_at' => '2022-01-21 10:35:43',
+                'user_id' => 1,
             ),
         ));
+        
+        
     }
 }
