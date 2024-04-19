04c9e6dcae7b797a8e6bf5c6ba98c420eb9316a4
Kamieljv <kamiel.verhelst@proton.me>
2024-04-19 16:42:07 +0200

    Small fix in displaying links and date/times

--- resources/views/acties/actie.blade.php
+++ resources/views/acties/actie.blade.php
@@ -87,7 +87,7 @@
                                     @endif
                                 </div>
                                 <div class="details-container text-sm text-gray-500">
-                                    @if($actie->start_time)
+                                    @if($actie->start_end)
                                         <div class="flex items-center text-sm leading-5 text-gray-700">
                                             @svg('antdesign-clock-circle-o', ['class' => 'shrink-0', 'style' => 'width: 20px; height: 20px'])
                                             &nbsp; <span class="font-medium">{{ $actie->start_end }}</span><br/>
@@ -121,7 +121,7 @@
                                                 $externe_link = 'https://' . $externe_link;
                                         ?>
                                         <a href="{{ $externe_link }}" target="_blank" class="w-full inline-flex items-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-[color:var(--wkid-pink)] hover:bg-[color:var(--wkid-pink-dark)]">
-                                            @svg('antdesign-link-o', ['style' => 'width: 20px; height: 20px']) &nbsp; {{ __('general.go_to')}} {{ $label[3] ?? $label[3]  }}
+                                            @svg('antdesign-link-o', ['style' => 'width: 20px; height: 20px']) &nbsp; {{ __('general.go_to')}} {{ $label == 1 ? $label[3] : $externe_link  }}
                                         </a>
                                     @endforeach
                                 </div>
@@ -131,10 +131,10 @@
                         <div class="sm:flex flex-col gap-3 not-prose p-3 bg-white rounded-lg shadow-lg overflow-hidden hidden">
                             <h4>{{ __("acties.details") }}</h4>
                             <div class="details-container text-sm text-gray-500">
-                                @if($actie->start_time)
+                                @if($actie->start_end)
                                     <div class="flex items-center text-sm leading-5 text-gray-700">
                                         @svg('antdesign-clock-circle-o', ['class' => 'shrink-0', 'style' => 'width: 20px; height: 20px'])
-                                        &nbsp; <span class="font-medium">{{ $actie->start_date . ' ' . $actie->start_time }}</span><br/>
+                                        &nbsp; <span class="font-medium">{{ $actie->start_end }}</span><br/>
                                     </div>
                                 @endif
                                 @if($actie->location_human)
@@ -152,7 +152,7 @@
                                             $externe_link = 'https://' . $externe_link;
                                     ?>
                                     <a href="{{ $externe_link }}" target="_blank" class="w-full inline-flex items-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-[color:var(--wkid-pink)] hover:bg-[color:var(--wkid-pink-dark)]">
-                                        @svg('antdesign-link-o', ['style' => 'width: 20px; height: 20px']) &nbsp; {{ __('general.go_to')}} {{ $label[3] ?? $label[3]  }}
+                                        @svg('antdesign-link-o', ['style' => 'width: 20px; height: 20px']) &nbsp; {{ __('general.go_to')}} {{ $label == 1 ? $label[3] : $externe_link }}
                                     </a>
                                 @endforeach
                             </div>
