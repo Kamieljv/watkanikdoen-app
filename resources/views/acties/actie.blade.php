ccc8276196ffd6d5fb931d1eee63ee363118df3e
Kamieljv <kamiel.verhelst@proton.me>
2024-04-21 15:28:21 +0200

    Third attempt to fix externe_link button

--- resources/views/acties/actie.blade.php
+++ resources/views/acties/actie.blade.php
@@ -149,11 +149,12 @@
                                 @foreach( $actie->externe_link as $externe_link )
                                     <?php
                                         preg_match( "/(http(s)?:[\\/]+)?([a-z0-9.\-_]+)[\\/]?/", strtolower($externe_link), $label );
-                                        if(substr( $externe_link, 0, 4) == 'http')
+                                        if(substr( $externe_link, 0, 4) != 'http') {
                                             $externe_link = 'https://' . $externe_link;
+                                        }
                                     ?>
                                     <a href="{{ $externe_link }}" target="_blank" class="w-full inline-flex items-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out border border-transparent rounded-md bg-[color:var(--wkid-pink)] hover:bg-[color:var(--wkid-pink-dark)]">
-                                        @svg('antdesign-link-o', ['style' => 'width: 20px; height: 20px']) &nbsp; {{ $label == 1 ? $label[3] : $externe_link }}
+                                        @svg('antdesign-link-o', ['style' => 'width: 20px; height: 20px']) &nbsp; {{ $label ? $label[3] : $externe_link }}
                                     </a>
                                 @endforeach
                             </div>
