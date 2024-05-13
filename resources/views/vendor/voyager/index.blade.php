41c00c79940fdc6997468175fe2845f8bd47915d
Kamieljv <kamiel.verhelst@proton.me>
2024-05-13 22:19:38 +0200

    Fixed umami stats in admin dashboard, added quick links

--- resources/views/vendor/voyager/index.blade.php
+++ resources/views/vendor/voyager/index.blade.php
@@ -12,6 +12,30 @@
     <div class="page-content container-fluid">
         @include('voyager::alerts')
 
+        <div class="col-md-12">
+            <div class="panel panel-bordered">
+                <div class="panel-body">
+                    <div class="flex border-0">
+                        <h2>Quick links</h2>
+                    </div>
+                    <div class="flex border-0">
+                        <a href="{{ route('voyager.acties.create') }}" class="quick-link">
+                            <button class="btn btn-primary" style="margin-right: 5px">
+                                <span style="font-size: 21px; font-weight: bold; line-height: 9px; margin-right: 2px;">+</span><i class="voyager-exclamation"></i>
+                                <span>Actie toevoegen</span>
+                            </button>
+                        </a>
+                        <a href="{{ route('voyager.organizers.create') }}" class="quick-link">
+                            <button class="btn btn-primary" style="margin-right: 5px">
+                                <span style="font-size: 21px; font-weight: bold; line-height: 9px; margin-right: 2px;">+</span><i class="voyager-pirate"></i>
+                                <span>Organizer toevoegen</span>
+                            </button>
+                        </a>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+
         <div id="app" class="stats-container">
             <stats-dashboard
                 umami-username="{{config('umami.username')}}"
