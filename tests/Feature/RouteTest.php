2aeed7a856f3a63a4733b339502eac2896d5ef23
Kamieljv <kamiel.verhelst@proton.me>
2024-12-03 18:46:36 +0100

    Update routetest to ignore vite

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -2,6 +2,9 @@
 
 use App\Models\User;
 
+beforeEach(fn () => $this->withoutVite());
+
+// Non-authenticated routes (defined in tests/Datasets/Routes.php)
 test('available routes', function ($url) {
 
     $appURL = env('APP_URL');
@@ -11,6 +14,7 @@ test('available routes', function ($url) {
     $response->assertStatus(200);
 })->with('routes');
 
+// Authenticated routes (defined in tests/Datasets/AuthRoutes.php)
 test('available auth routes', function ($url) {
 
     $user = User::find(1);
