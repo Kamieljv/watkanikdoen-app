36f9b01efbaed471b2fd771430ff6e36ef24bfcf
Kamieljv <kamiel.verhelst@proton.me>
2025-03-03 13:41:17 +0100

    Comment out full calendar method (generate)

--- app/Http/Controllers/ICalController.php
+++ app/Http/Controllers/ICalController.php
@@ -10,56 +10,56 @@ use \App\Models\Actie;
 class ICalController extends Controller
 {
 
-    /**
-     * Gets the events data from the database
-     * and populates the iCal object.
-     *
-     * @return void
-     */
-    public function generate($id = null)
-    {
-        if ($id === null) {
-            $events = Actie::published()->nietAfgelopen()->get();
-        } else {
-            $events = Actie::findOrFail($id);
-            if ($events === false) {
-                dd($events);
-            }
-        }
+    // /**
+    //  * Gets the events data from the database
+    //  * and populates the iCal object.
+    //  *
+    //  * @return void
+    //  */
+    // public function generate($id = null)
+    // {
+    //     if ($id === null) {
+    //         $events = Actie::published()->nietAfgelopen()->get();
+    //     } else {
+    //         $events = Actie::findOrFail($id);
+    //         if ($events === false) {
+    //             dd($events);
+    //         }
+    //     }
 
-        define('ICAL_FORMAT', 'Ymd\THis\Z');
+    //     define('ICAL_FORMAT', 'Ymd\THis\Z');
 
-        $icalObject = "BEGIN:VCALENDAR
-       VERSION:2.0
-       METHOD:PUBLISH
-       PRODID:-//Watkanikdoen.nl//Acties//NL\n";
+    //     $icalObject = "BEGIN:VCALENDAR
+    //    VERSION:2.0
+    //    METHOD:PUBLISH
+    //    PRODID:-//Watkanikdoen.nl//Acties//NL\n";
 
-        // loop over events
-        foreach ($events as $event) {
-            $icalObject .=
-            "BEGIN:VEVENT
-           DTSTART:" . date(ICAL_FORMAT, strtotime($event->starts)) . "
-           DTEND:" . date(ICAL_FORMAT, strtotime($event->ends)) . "
-           DTSTAMP:" . date(ICAL_FORMAT, strtotime($event->created_at)) . "
-           SUMMARY:$event->excerpt
-           UID:$event->id
-           STATUS:" . strtoupper($event->status) . "
-           LAST-MODIFIED:" . date(ICAL_FORMAT, strtotime($event->updated_at)) . "
-           LOCATION:$event->location
-           END:VEVENT\n";
-        }
+    //     // loop over events
+    //     foreach ($events as $event) {
+    //         $icalObject .=
+    //         "BEGIN:VEVENT
+    //        DTSTART:" . date(ICAL_FORMAT, strtotime($event->starts)) . "
+    //        DTEND:" . date(ICAL_FORMAT, strtotime($event->ends)) . "
+    //        DTSTAMP:" . date(ICAL_FORMAT, strtotime($event->created_at)) . "
+    //        SUMMARY:$event->excerpt
+    //        UID:$event->id
+    //        STATUS:" . strtoupper($event->status) . "
+    //        LAST-MODIFIED:" . date(ICAL_FORMAT, strtotime($event->updated_at)) . "
+    //        LOCATION:$event->location
+    //        END:VEVENT\n";
+    //     }
 
-        // close calendar
-        $icalObject .= "END:VCALENDAR";
+    //     // close calendar
+    //     $icalObject .= "END:VCALENDAR";
 
-        // Set the headers
-        header('Content-type: text/calendar; charset=utf-8');
-        header('Content-Disposition: attachment; filename="cal.ics"');
+    //     // Set the headers
+    //     header('Content-type: text/calendar; charset=utf-8');
+    //     header('Content-Disposition: attachment; filename="cal.ics"');
 
-        $icalObject = str_replace(' ', '', $icalObject);
+    //     $icalObject = str_replace(' ', '', $icalObject);
 
-        echo $icalObject;
-    }
+    //     echo $icalObject;
+    // }
 
     public function actie($slug)
     {
