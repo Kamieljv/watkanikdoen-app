846367fdec18e575d8b4ae56348e78039c980540
Kamieljv <kamiel.verhelst@proton.me>
2025-09-02 21:46:42 +0200

    Fix themeschips justify

--- resources/views/assets/js/components/partials/ThemesChips.vue
+++ resources/views/assets/js/components/partials/ThemesChips.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul v-if="themes.length <= maxVisible" class="flex flex-wrap">
+    <ul v-if="themes.length <= maxVisible" :class="`flex flex-wrap justify-${justify}`">
         <li v-for="theme in themes" :key="theme.id"
             class="relative self-start inline-block px-2 py-1 mr-1 mb-1 text-xs font-medium leading-5 text-white uppercase bg-gray-100 rounded"
             :style="{ backgroundColor: theme.color }">
@@ -8,7 +8,7 @@
             </span>
         </li>
     </ul>
-    <ul v-else class="flex flex-wrap">
+    <ul v-else :class="`flex flex-wrap justify-${justify}`">
         <li v-for="theme in themes.slice(0, maxVisible)" class="relative self-start inline-block px-2 py-1 mr-1 mb-1 text-xs font-medium leading-5 text-white uppercase bg-gray-100 rounded"
             :style="{ backgroundColor: themes[0].color }">
             <span class="text-white" rel="theme">
@@ -39,6 +39,10 @@ export default {
         showPlusLabel: {
             type: Boolean,
             default: true,
+        },
+        justify: {
+            type: String,
+            default: 'start',
         },
     },
 };
