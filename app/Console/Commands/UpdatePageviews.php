f9918514ad4bbaba2a572f60f4d58420d7ef10f1
Kamiel Verhelst <kamiel.verhelst@proton.me>
2026-01-24 11:53:17 +0100

    Made updatepageviews command more robust

--- app/Console/Commands/UpdatePageviews.php
+++ app/Console/Commands/UpdatePageviews.php
@@ -3,9 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Models\Actie;
+use App\Notifications\Mail\ErrorAlert;
 use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Notification;
 use Jenssegers\Date\Date;
 
 
@@ -42,35 +44,46 @@ class UpdatePageviews extends Command
      */
     public function handle()
     {
-        // Login and get an authorization token
-        $response = Http::post(config('umami.url') . '/api/auth/login', [
-            "username" => config('umami.username'),
-            "password" => config('umami.password'),
-        ]);
-        // save token
-        $token = json_decode($response->body())->token;
-        // make metrics request
-        $response = Http::withToken($token)->get(config('umami.url') . '/api/websites/' . config('umami.websiteId') . '/metrics', [
-            'startAt' => Date::createFromDate(2000, 01, 01)->timestamp * 1000,
-            'endAt' => Date::now()->timestamp * 1000,
-            'type' => 'url'
-        ]);
-        // declare pagestats and filter for acties only
-        $pageStats = json_decode($response->body());
-        $actieStats = Arr::where($pageStats, function($v, $k) {
-            return preg_match('/\/actie\//', $v->x);
-        });
-        // Update pageviews for all found acties
-        foreach ($actieStats as $stat) {
-            $slug = preg_split('/\/actie\//', $stat->x)[1];
-            $actie = Actie::where('slug', $slug)->first();
-            if ($actie != null) {
-                $actie->pageviews = $stat->y;
-                // disable and re-enable timestamps to not update the updated_at field when updating pageviews
-                $actie->timestamps = false;
-                $actie->save();
-                $actie->timestamps = true;
+        try {
+            // Login and get an authorization token
+            $response = Http::post(config('umami.url') . '/api/auth/login', [
+                "username" => config('umami.username'),
+                "password" => config('umami.password'),
+            ]);
+            // save token
+                $response->throw();
+                $token = json_decode($response->body())->token;
+            
+            // make metrics request
+            $response = Http::withToken($token)->get(config('umami.url') . '/api/websites/' . config('umami.websiteId') . '/metrics', [
+                'startAt' => Date::createFromDate(2000, 01, 01)->timestamp * 1000,
+                'endAt' => Date::now()->timestamp * 1000,
+                'type' => 'url'
+            ]);
+            // declare pagestats and filter for acties only
+            $pageStats = json_decode($response->body());
+            $actieStats = Arr::where($pageStats, function($v, $k) {
+                return preg_match('/\/actie\//', $v->x);
+            });
+            // Update pageviews for all found acties
+            foreach ($actieStats as $stat) {
+                $slug = preg_split('/\/actie\//', $stat->x)[1];
+                $actie = Actie::where('slug', $slug)->first();
+                if ($actie != null) {
+                    $actie->pageviews = $stat->y;
+                    // disable and re-enable timestamps to not update the updated_at field when updating pageviews
+                    $actie->timestamps = false;
+                    $actie->save();
+                    $actie->timestamps = true;
+                }
             }
+            return 0;
+        } catch (\Exception $e) {
+            $this->error('Failed to authenticate with Umami: ' . $e->getMessage());
+            // Send email to admin about failure
+            Notification::route('mail', config('app.admin_email'))
+                ->notify((new ErrorAlert("Unable to update pageviews. Error: " . $e->getMessage()))->delay(now()->addSeconds(5)));
+            return 1;
         }
     }
 }
