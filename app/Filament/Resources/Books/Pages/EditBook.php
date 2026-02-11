60ef5cd0843bade8b42df88f44dc22606c0a4496
Kamiel Verhelst <kamiel.verhelst@proton.me>
2026-02-11 20:20:50 +0100

    Add book resource and ISBN search functionality with APIs

--- app/Filament/Resources/Books/Pages/EditBook.php
+++ app/Filament/Resources/Books/Pages/EditBook.php
@@ -16,4 +16,9 @@ class EditBook extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
