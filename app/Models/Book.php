4da67d78eb0614eb292e48eb525e281e2d24d18f
Kamiel Verhelst <kamiel.verhelst@proton.me>
2026-02-12 10:33:57 +0100

    Add themes and tags to books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,19 +3,28 @@
 namespace App\Models;
 
 use App\Models\Resource;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Book extends Resource
 {
     /**
-     * Create a new Eloquent model instance.
-     *
-     * @param  array  $attributes
-     * @return void
+     * Add fillable attributes to the base Resource model
+     * @param array $attributes
      */
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
 
         $this->mergeFillable(['isbn', 'cover_image']);
+    }
+
+    public function themes()
+    {
+        return $this->belongsToMany(Theme::class );
+    }
+
+    public function tags(): MorphToMany
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
     }
 }
