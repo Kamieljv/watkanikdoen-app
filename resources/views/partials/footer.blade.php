711a3d9414266831942358c0206b4d9aa5d9937f
Kamiel Verhelst <kamiel.verhelst@proton.me>
2026-05-18 21:36:49 +0200

    Updated menus

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="bg-gray-700 text-white">
     <div class="px-8 pt-16 mx-auto lg:px-12 xl:px-16 max-w-6xl">
-        <div class="flex flex-col gap-3 items-center justify-between py-10 border-t border-solid sm:flex-row border-white/50">
+        <div
+            class="flex flex-col gap-3 items-center justify-between py-10 border-t border-solid sm:flex-row border-white/50">
             <a id="footer-logo-link" href="{{ route('home') }}">
                 @svg('custom-logo-icon', ['style' => 'fill: currentColor; height: 32px;'])
             </a>
@@ -24,12 +25,18 @@
                         </a>
                     </li>
                     <li class="text-center">
-                        <a href="{{ config('app.donate_link') }}" class="relative inline-block group" style="color: inherit;" target="_blank">
+                        <a href="/nieuwsbrief" class="relative inline-block group" style="color: inherit;">
+                            <span>{{ __('menus.newsletter') }}</span>
+                        </a>
+                    </li>
+                    <li class="text-center">
+                        <a href="{{ config('app.donate_link') }}" class="relative inline-block group"
+                            style="color: inherit;" target="_blank">
                             <span>{{ __('menus.donate') }}</span>
                         </a>
                     </li>
                     <li class="text-center">
-                        <a href="word-vrijwilliger" class="relative inline-block group" style="color: inherit;">
+                        <a href="/word-vrijwilliger" class="relative inline-block group" style="color: inherit;">
                             <span>{{ __('menus.word_vrijwilliger') }}</span>
                         </a>
                     </li>
@@ -55,8 +62,7 @@
                     <a href="https://bsky.app/profile/watkanikdoen-nl.bsky.social"
                         class="text-white hover:text-gray-400">
                         <span class="sr-only">Bluesky</span>
-                        <svg class="w-5 h-5" viewBox="0 0 24 24" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
+                        <svg class="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M12 11.3884C11.0942 9.62673 8.62833 6.34423 6.335 4.7259C4.13833 3.17506 3.30083 3.4434 2.75167 3.69256C2.11583 3.9784 2 4.95506 2 5.52839C2 6.10339 2.315 10.2367 2.52 10.9276C3.19917 13.2076 5.61417 13.9776 7.83917 13.7309C4.57917 14.2142 1.68333 15.4017 5.48083 19.6292C9.65833 23.9542 11.2058 18.7017 12 16.0392C12.7942 18.7017 13.7083 23.7651 18.4442 19.6292C22 16.0392 19.4208 14.2142 16.1608 13.7309C18.3858 13.9784 20.8008 13.2076 21.48 10.9276C21.685 10.2376 22 6.10256 22 5.52923C22 4.95423 21.8842 3.97839 21.2483 3.6909C20.6992 3.44256 19.8617 3.17423 17.665 4.72423C15.3717 6.34506 12.9058 9.62756 12 11.3884Z">
                             </path>
