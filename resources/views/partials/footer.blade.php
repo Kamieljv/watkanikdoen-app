2d162df27ecdb7f0d4c0faa2aab481ed693c5482
Kamiel Verhelst <kamiel.verhelst@proton.me>
2026-05-18 21:08:18 +0200

    Update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,40 +1,45 @@
 <footer class="bg-gray-700 text-white">
     <div class="px-8 pt-16 mx-auto lg:px-12 xl:px-16 max-w-6xl">
-        <div class="flex flex-col items-center justify-between py-10 border-t border-solid sm:flex-row border-gray">
+        <div class="flex flex-col gap-3 items-center justify-between py-10 border-t border-solid sm:flex-row border-white/50">
             <a id="footer-logo-link" href="{{ route('home') }}">
                 @svg('custom-logo-icon', ['style' => 'fill: currentColor; height: 32px;'])
             </a>
-            <ul
-                class="flex flex-wrap flex-col space-y-3 space-x-0 sm:space-y-0 sm:flex-row sm:space-x-5 text-xs my-5 sm:my-0">
-                <li class="text-center flex-full sm:flex-none sm:mb-0">&copy; {{ date('Y') }}
-                    {{ config('brand.title') }}</li>
-                <li class="text-center">
-                    <a href="/algemene-voorwaarden-en-privacyverklaring" class="relative inline-block group"
-                        style="color: inherit;">
-                        <span>{{ __('Terms and Privacypolicy') }}</span>
-                    </a>
-                </li>
-                <li class="text-center">
-                    <a href="/welke-acties-plaatsen-we" class="relative inline-block group" style="color: inherit;">
-                        <span>{{ __('menus.welke_acties_plaatsen_we') }}</span>
-                    </a>
-                </li>
-                <li class="text-center">
-                    <a href="/over-ons" class="relative inline-block group" style="color: inherit;">
-                        <span>{{ __('menus.about_us') }}</span>
-                    </a>
-                </li>
-                <li class="text-center">
-                    <a href="{{ config('app.donate_link') }}" class="relative inline-block group" style="color: inherit;" target="_blank">
-                        <span>{{ __('menus.donate') }}</span>
-                    </a>
-                </li>
-                <li class="text-center">
-                    <a href="/contact" class="relative inline-block group" style="color: inherit;">
-                        <span>{{ __('menus.contact') }}</span>
-                    </a>
-                </li>
-            </ul>
+            <div class="flex-1 flex items-center justify-center">
+                <ul
+                    class="flex flex-wrap flex-col sm:gap-y-2 gap-y-3 space-x-0 sm:justify-center sm:flex-row sm:space-x-5 text-xs my-5 sm:my-0">
+                    <li class="text-center">
+                        <a href="/algemene-voorwaarden-en-privacyverklaring" class="relative inline-block group"
+                            style="color: inherit;">
+                            <span>{{ __('Terms and Privacypolicy') }}</span>
+                        </a>
+                    </li>
+                    <li class="text-center">
+                        <a href="/welke-acties-plaatsen-we" class="relative inline-block group" style="color: inherit;">
+                            <span>{{ __('menus.welke_acties_plaatsen_we') }}</span>
+                        </a>
+                    </li>
+                    <li class="text-center">
+                        <a href="/over-ons" class="relative inline-block group" style="color: inherit;">
+                            <span>{{ __('menus.about_us') }}</span>
+                        </a>
+                    </li>
+                    <li class="text-center">
+                        <a href="{{ config('app.donate_link') }}" class="relative inline-block group" style="color: inherit;" target="_blank">
+                            <span>{{ __('menus.donate') }}</span>
+                        </a>
+                    </li>
+                    <li class="text-center">
+                        <a href="word-vrijwilliger" class="relative inline-block group" style="color: inherit;">
+                            <span>{{ __('menus.word_vrijwilliger') }}</span>
+                        </a>
+                    </li>
+                    <li class="text-center">
+                        <a href="/contact" class="relative inline-block group" style="color: inherit;">
+                            <span>{{ __('menus.contact') }}</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
             <ul class="flex items-center space-x-5 lg:mt-0">
                 <li>
                     <a href="https://www.instagram.com/watkanikdoen.nl/" class="text-white hover:text-gray-400">
@@ -69,14 +74,6 @@
                     </a>
                 </li>
             </ul>
-        </div>
-        <div class="flex flex-col items-center justify-center pb-10 sm:flex-row">
-            <div>
-                <p class="text-xs pb-4">Dit initiatief wordt gesteund door: </p>
-                <a id="footer-fnv" href="https://youngandunited.nl">
-                    @svg('custom-fnv-yu', ['style' => 'fill: currentColor; height: 50px;'])
-                </a>
-            </div>
         </div>
     </div>
 </footer>
